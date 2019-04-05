e4787f7ded8d66825a660c5c846a4e11c01a576f
Sebastián Gurín <sgurin@awalabs.com>
2019-04-05 17:56:01 -0300

    project diff

--- src/cli/diff/projectDiff.ts
+++ src/cli/diff/projectDiff.ts
@@ -14,11 +14,18 @@ import { ansi } from 'cli-driver'
 registerPrompt('checkbox-plus', require('inquirer-checkbox-plus-prompt'))
 
 export async function showProjectDiff(project: Project) {
-  const allFiles = project.getSourceFiles().map(f => ({
+  const allFiles = project.getSourceFiles()
+  .map(f => ({
     oldText: readFileSync(f.getFilePath()).toString(),
     newText: f.getFullText(),
     name: getFileRelativePath(f, project)
   }))
+  .filter(f=>f.newText!==f.oldText)
+  .map(file => ({
+    name: file.name,
+    diff: createPatch(file.name, file.oldText, file.newText)
+  }))
+  
   // const {files} = await prompt<File[]>([{
   //   type: 'checkbox-plus',
   //   name: 'files',
@@ -32,22 +39,46 @@ export async function showProjectDiff(project: Project) {
   //   }
   // }])
 
-  allFiles.filter(f=>f.newText!==f.oldText).map(file => ({
-    name: file.name,
-    diff: createPatch(file.name, file.oldText, file.newText)
-  }))
-    .forEach(f => {
-      console.log(`
-${f.name}
+  // allFiles.)
+  // allFiles.forEach(f => {
+//       console.log(`
+// ${f.name}
 
-${colorizeDiff(f.diff)}
-    `);
+// ${colorizeDiff(f.diff)}
+//     `);
 
-    })
+//     })
 
+let answer = {selection: {}}
+while(answer.selection!=='__goBack__'){
+  answer = await prompt<{selection: string|{name:string, diff: string}}>([{
+    type: 'list',name: 'selection', message: 'Choose a file to see its diff', choices: [
+      {name: 'Go back', value: '__goBack__'}, ...allFiles.map(f=>({name: f.name, value: f}))
+    ]
+  }])
+  if(answer.selection!=='__goBack__'){
+    showDiff(answer.selection as any as {name:string, diff: string})
+  }
+}
     
 }
+function showDiff(f: {name:string, diff: string}){
+  console.log(`${colorizeDiff(f.diff)}`);
+}
 
+// async function selectFiles(allFiles: {name:string, diff: string}[]){
+//   let answer = {selection: {}}
+//   while(answer.selection!=='__goBack__'){
+//     answer = await prompt<{selection: string|{name:string, diff: string}}>([{
+//       type: 'list',name: 'selection', message: 'Choose a file to see its diff', choices: [
+//         {name: 'Go back', value: '__goBack__'}, ...allFiles.map(f=>({name: f.name, value: f}))
+//       ]
+//     }])
+//     if(answer.selection!=='__goBack__'){
+//       showDiff(answer.selection as any as {name:string, diff: string})
+//     }
+//   }
+// }
 function colorizeDiff(p: string) {
   return p.split('\n').map(l => l.startsWith('-') ? ansi.format(l, ['red']) : l.startsWith('+') ? ansi.format(l, ['green']) : l).join('\n')
 }
